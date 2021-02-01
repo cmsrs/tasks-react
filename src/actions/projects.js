9ed0d2605b50f59c677ea0024e276ee504768ee0
rob <robert.szczepanski@cmsrs.pl>
2021-02-01 15:40:20 +0100

    project adn tasks v2

--- src/actions/projects.js
+++ src/actions/projects.js
@@ -88,168 +88,45 @@ export const saveTask = (task, callback) => async  dispatch => {
   }
 };
 
+export const deleteProject = (projectId, callback) =>  async dispatch => {
+
+  const token = localStorage.getItem('token');
+
+  try{
+    const response = await axios.delete(
+      SERVER_URL + '/api/projects/'+projectId+'?token='+token
+    );
+
+    if(!response.data.success){
+      dispatch({ type: PROJECTS_RES, payload: {success: false, message: response.data.error} });
+    }else{
+      //dispatch({ type: PROJECTS_RES, payload: {success: true, message: "Data was saved"} });
+      callback();
+    }
+
+  } catch(e){
+     dispatch({ type: PROJECTS_RES, payload: {success: false, message: "Unknown problem with ajax, while deleteing proj"} });
+  }
+}
 
 
+export const deleteTask = (taskId, callback) =>  async dispatch => {
 
+  const token = localStorage.getItem('token');
 
-//
-//
-//
-// export const getConfig = (callback) => async dispatch => {
-//   const token = localStorage.getItem('token');
-//
-//   try {
-//     const response = await axios.get(
-//       prefixUrl+'/config?token='+token
-//     );
-//     dispatch({ type: CONFIG_GET_CONFIG, payload: response.data.data });
-//     callback(response.data.data);
-//
-//   } catch(e){
-//      dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Unknown problem with ajax, while get config"} });
-//   }
-// };
-//
-// export const changePosition = (direction, itemId, menusOrPagesOrImg, callback) => async dispatch => {
-//   const token = localStorage.getItem('token');
-//   try{
-//     const response = await axios.get(
-//       prefixUrl+'/'+menusOrPagesOrImg+'/position/'+direction+'/'+itemId+'?token='+token
-//     );
-//
-//     if(!response.data.success){
-//       dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Problem with change position "+menusOrPagesOrImg} });
-//     }else{
-//       dispatch({ type: PRODUCTS_RES, payload: {success: true, message: "Data was saved"} });
-//       callback();
-//     }
-//
-//
-//   } catch(e){
-//      dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Unknown problem with ajax, while change "+menusOrPagesOrImg+" position"} });
-//   }
-// }
-//
-// export const delImage = (imageId, callback) => async dispatch => {
-//   const token = localStorage.getItem('token');
-//
-//   try{
-//     const response = await axios.delete(
-//       prefixUrl+'/images/'+imageId+'?token='+token
-//     );
-//
-//     if(!response.data.success){
-//       dispatch({ type: PRODUCTS_RES, payload: {success: false, message: response.data.error} });
-//     }else{
-//       dispatch({ type: PRODUCTS_RES, payload: {success: true, message: "Data was saved"} });
-//       callback();
-//     }
-//
-//
-//   } catch(e){
-//      dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Unknown problem with ajax, while deleteing image"} });
-//   }
-// }
-//
-//
-//
-// export const getShopPages = () => async dispatch => {
-//   const token = localStorage.getItem('token');
-//
-//   try {
-//     const response = await axios.get(
-//       prefixUrl+'/pages/type/shop?token='+token
-//     );
-//     dispatch({ type: PRODUCTS_GET_PAGES, payload: response.data.data });
-//     //callback();
-//
-//   } catch(e){
-//      dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Unknown problem with ajax, while get pages for products"} });
-//   }
-// };
-//
-//
-// export const getProducts = (callback) => async dispatch => {
-//   const token = localStorage.getItem('token');
-//
-//   try {
-//     const response = await axios.get(
-//       prefixUrl+'/products?token='+token
-//     );
-//
-//     if(response.data.success){
-//       dispatch({ type: PRODUCTS_GET_PRODUCTS, payload: response.data.data });
-//       callback(response.data.data);
-//     }else{
-//       dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Unknown problem with ajax, while get products0"} });
-//     }
-//
-//   } catch(e){
-//      dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Unknown problem with ajax, while get products"} });
-//   }
-// };
-//
-// export const saveProduct = (product, callback) => async  dispatch => {
-//
-//   const token = localStorage.getItem('token');
-//
-//   try {
-//     let response = null;
-//     if( product.id ){
-//       response = await axios.put(
-//         prefixUrl+'/products/'+product.id+'?token='+token,
-//         product
-//       );
-//     }else{
-//       response = await axios.post(
-//         prefixUrl+'/products?token='+token,
-//         product
-//       );
-//     }
-//
-//     if(!response.data.success){
-//       dispatch({ type: PRODUCTS_RES, payload: {success: false, message: response.data.error} });
-//     }else{
-//       const productId = product.id ? product.id : response.data.data.productId; //update
-//       dispatch({ type: PRODUCTS_RES, payload: {success: true, message: "Data was saved"} });
-//       dispatch({ type: PRODUCTS_SAVE_PRODUCT, payload: product });
-//       callback(productId);
-//     }
-//
-//   } catch (e) {
-//      console.log('___probem with ajax______', e);
-//      dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Unknown problem with ajax, while save page"} });
-//   }
-// };
-//
-//
-// export const deleteProduct = (productId) =>  async dispatch => {
-//
-//   const token = localStorage.getItem('token');
-//
-//   try{
-//     const response = await axios.delete(
-//       prefixUrl+'/products/'+productId+'?token='+token
-//     );
-//
-//     if(!response.data.success){
-//       dispatch({ type: PRODUCTS_RES, payload: {success: false, message: response.data.error} });
-//     }else{
-//       dispatch({ type: PRODUCTS_RES, payload: {success: true, message: "Data was saved"} });
-//     }
-//
-//     dispatch({ type: PRODUCTS_DELETE_PRODUCT, payload: productId });
-//   } catch(e){
-//      dispatch({ type: PRODUCTS_RES, payload: {success: false, message: "Unknown problem with ajax, while deleteing product"} });
-//   }
-// }
-//
-// //not used - to del - use only changeProduct
-// export const setProduct = (product) => dispatch => {
-//   dispatch({ type: PRODUCTS_SET_PRODUCT, payload: product });
-// };
-//
-//
-// export const changeProduct = (product) => dispatch => {
-//   dispatch({ type: PRODUCTS_CHANGE_PRODUCT, payload: product });
-// };
+  try{
+    const response = await axios.delete(
+      SERVER_URL + '/api/tasks/'+taskId+'?token='+token
+    );
+
+    if(!response.data.success){
+      dispatch({ type: PROJECTS_RES, payload: {success: false, message: response.data.error} });
+    }else{
+      //dispatch({ type: PROJECTS_RES, payload: {success: true, message: "Data was saved"} });
+      callback();
+    }
+
+  } catch(e){
+     dispatch({ type: PROJECTS_RES, payload: {success: false, message: "Unknown problem with ajax, while deleteing task"} });
+  }
+}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/projects';
 import Header from './Header';
 import Expire from '../helpers/Expire';
+import './main.css';
 
 class Projects extends Component {
 
@@ -42,13 +43,28 @@ class Projects extends Component {
     this.setState({projTitle: title});
   }
 
+  handleChangeTitleShow = (event) => {
+    let title = event.target.value;
+    const projectid = parseInt(event.target.attributes.getNamedItem('data-projectid').value.replace( /^\D+/g, ''));
+
+    let i = 0;
+    let newProj = [];
+    for(let obj of this.state.projects){
+      let copy = Object.assign({}, obj);
+      if( obj.id === projectid ){
+        copy.title = title;
+      }
+      newProj.push(copy);
+    }
+    this.setState({projects: newProj});
+  }
+
   handleAddTask = () => {
     const name = this.state.taskName;
     const points = this.state.taskPoints;
     const projectId = this.state.taskProjectId;
 
     const task = {name: name, points: points, project_id: projectId};
-    console.log(task);
     this.props.saveTask(task , () => {
       this.props.getProjects( (d) => {
         this.setState({projects: d});
@@ -68,16 +84,117 @@ class Projects extends Component {
     this.setState({taskPoints: points});
   }
 
+  handleChangeNameShow = (event) => {
+    const name = event.target.value;
+    const projectid = parseInt(event.target.attributes.getNamedItem('data-projectid').value.replace( /^\D+/g, ''));
+    const taskid = parseInt(event.target.attributes.getNamedItem('data-taskid').value.replace( /^\D+/g, ''));
+
+    let newProj = [];
+    for(let obj of this.state.projects){
+      let copyProj = Object.assign({}, obj);
+      if( obj.id === projectid ){
+        let newTasks = [];
+        for(let task of obj.tasks){
+          let copyTask = Object.assign({}, task);
+          if( task.id === taskid ){
+            copyTask.name = name;
+          }
+          newTasks.push(copyTask);
+        }
+        copyProj.tasks = newTasks;
+      }
+      newProj.push(copyProj);
+    }
+    this.setState({projects: newProj});
+  }
+
+  //dry - todo
+  handleChangePointsShow = (event) => {
+    const points = event.target.value;
+    const projectid = parseInt(event.target.attributes.getNamedItem('data-projectid').value.replace( /^\D+/g, ''));
+    const taskid = parseInt(event.target.attributes.getNamedItem('data-taskid').value.replace( /^\D+/g, ''));
+
+    let newProj = [];
+    for(let obj of this.state.projects){
+      let copyProj = Object.assign({}, obj);
+      if( obj.id === projectid ){
+        let newTasks = [];
+        for(let task of obj.tasks){
+          let copyTask = Object.assign({}, task);
+          if( task.id === taskid ){
+            copyTask.points = points;
+          }
+          newTasks.push(copyTask);
+        }
+        copyProj.tasks = newTasks;
+      }
+      newProj.push(copyProj);
+    }
+    this.setState({projects: newProj});
+  }
+
+
   handleChangeProjectId = (event) => {
     let projectId = event.target.value
     //console.log('proij_id='+projectId);
     this.setState({taskProjectId : projectId});
   }
 
-  render() {
+  editProj = (projectid) => {
+    let copyProj = '';
+    for(let obj of this.state.projects){
+      if( obj.id === projectid ){
+        copyProj = {id:obj.id, title: obj.title};
+        break;
+      }
+    }
+    if(copyProj){
+      this.props.saveProject(copyProj , () => {
+        this.props.getProjects( (d) => {
+          this.setState({projects: d});
+        });
+      });
+    }
+  }
 
+  delProj = (projectId) => {
+
+    this.props.deleteProject(projectId, () => {
+      this.props.getProjects( (d) => {
+        this.setState({projects: d});
+      });
+    });
+  }
+
+  delTask = (taskId) => {
+    this.props.deleteTask(taskId, () => {
+      this.props.getProjects( (d) => {
+        this.setState({projects: d});
+      });
+    });
+  }
+
+  editTask = ( taskId ) => {
+    let copyTask = '';
+    for(let obj of this.state.projects){
+      for(let task of obj.tasks){
+        if( task.id === taskId ){
+          copyTask = {id:task.id, name: task.name, points:task.points, project_id: task.project_id };
+        }
+      }
+    }
+    if(copyTask){
+      this.props.saveTask(copyTask, () => {
+        this.props.getProjects( (d) => {
+          this.setState({projects: d});
+        });
+      });
+    }
+  }
+
+
+  render() {
     const projects =  this.state.projects;
-    console.log(projects);
 
     let msg = '';
     if(this.props.projectsRes && (this.props.projectsRes.success  === false)  ){
@@ -115,24 +232,49 @@ class Projects extends Component {
           <div className="row mt-4">
             <input type="text" placeholder="Project title" name="title" key="title" className="mr-2"
                       onChange={this.handleChangeTitle}  value={this.state.projTitle} />
-            <button className="btn btn-primary mt-2 mb-2"  onClick={this.handleAddProject}><i className="fas fa-plus"></i> Add project</button>
+            <button className="btn btn-primary mt-2 mb-2"  onClick={this.handleAddProject}>Add project</button>
           </div>
 
           { projects.length &&
-          <div className="row mt-3">
-            <input type="text" placeholder="Task name" name="name" key="name" className="mr-2"
-                      onChange={this.handleChangeName}  value={this.state.taskName} />
-            <input type="text" placeholder="Task points" name="points" key="points" className="mr-2"
-                      onChange={this.handleChangePoints}  value={this.state.taskPoints} />
-            <select name="project_id" onChange={this.handleChangeProjectId}   className="mr-2" value={this.state.taskProjectId} >
-                {projects.map(project =>
-                  <option key={project.id} value={project.id || ''}>{project.title || ''}</option>
-                )}
-            </select>
+            <div className="row mt-3">
+              <input type="text" placeholder="Task name" name="name" key="name" className="mr-2"
+                        onChange={this.handleChangeName}  value={this.state.taskName} />
+              <input type="text" placeholder="Task points" name="points" key="points" className="mr-2"
+                        onChange={this.handleChangePoints}  value={this.state.taskPoints} />
+              <select name="project_id" onChange={this.handleChangeProjectId}   className="mr-2" value={this.state.taskProjectId} >
+                  {projects.map(project =>
+                    <option key={project.id} value={project.id || ''}>{project.title || ''}</option>
+                  )}
+              </select>
 
-            <button id="add_menu" className="btn btn-primary mt-2 mb-2"  onClick={this.handleAddTask}><i className="fas fa-plus"></i> Add task</button>
-          </div>
+              <button id="add_menu" className="btn btn-primary mt-2 mb-2"  onClick={this.handleAddTask}>Add task</button>
+            </div>
           }
+
+          <div className="row mt-3">
+          { projects.length && projects.map(project =>
+            <div  className="mt-3  container" key={'wrap_'+project.id}>
+              <input type="text"  key={'proj_'+project.id} data-projectid={'project_id_'+project.id} placeholder="Project title" name="title" key="title" className="m-2"
+                        onChange={this.handleChangeTitleShow}  value={project.title} />
+                        <span onClick={this.editProj.bind(this, project.id)} ><i className="m-1 far fa-edit cursor-pointer"/></span>
+                        <span onClick={this.delProj.bind(this, project.id)}><i className="m-1 fas fa-trash cursor-pointer"  aria-hidden="true"/></span>
+              <ul>
+              {project.tasks.map(task =>
+                  <li key={'wrap_task_'+project.id+'_'+task.id}>
+                    <input type="text" data-projectid={'project_id_'+project.id} data-taskid={'task_id_'+task.id}  placeholder="Task name" name="name" key="name" className="m-1"
+                              onChange={this.handleChangeNameShow}  value={task.name} />
+                    <input type="text" data-projectid={'project_id_'+project.id} data-taskid={'task_id_'+task.id}  placeholder="Task points" name="points" key="points" className="m-1"
+                              onChange={this.handleChangePointsShow}  value={task.points} />
+
+                    <span onClick={this.editTask.bind(this, task.id)}><i className="m-1 far fa-edit cursor-pointer"/></span>
+                    <span onClick={this.delTask.bind(this, task.id)}><i className="m-1 fas fa-trash cursor-pointer"  aria-hidden="true"/></span>
+                  </li>
+              )}
+              </ul>
+            </div>
+          )}
+          </div>
+
 
         </div>
       </div>
